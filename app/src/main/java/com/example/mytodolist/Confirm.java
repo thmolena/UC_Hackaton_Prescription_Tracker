56bc77683abda69e6545406c01a0a4eb4b67e767
nguyent2 <nguyent2@berea.edu>
2020-02-23 11:46:15 -0500

    Completed project

---
+++ app/src/main/java/com/example/mytodolist/Confirm.java
@@ -0,0 +1,20 @@
+package com.example.mytodolist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.support.v7.app.AppCompatActivity;
+
+public class Confirm extends AppCompatActivity {
+Button btnsend;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
--- app/src/main/java/com/example/mytodolist/DoctorPageSearch.java
+++ app/src/main/java/com/example/mytodolist/DoctorPageSearch.java
@@ -11,7 +11,6 @@ import android.widget.Toast;
 public class DoctorPageSearch extends AppCompatActivity {
 
     Button signout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +18,10 @@ public class DoctorPageSearch extends AppCompatActivity {
 
         //Find our button. It's just a regular button center in the screen.
         signout = findViewById(R.id.signout);
-
         //Set an onClickListener to detect clicks.
+
         signout.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 //Get the intent from the previous page.
@@ -38,5 +38,30 @@ public class DoctorPageSearch extends AppCompatActivity {
             }
         });
 
+        Button btnsend;
+            //Find our button. It's just a regular button center in the screen.
+            btnsend = findViewById(R.id.btnsend);
+
+            //Set an onClickListener to detect clicks.
+            btnsend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Get the intent from the previous page.
+                    Intent receiveIntent = getIntent();
+                    //Get a Bundle of data from the previous page rather than receive each string individually.
+                    Bundle bundle = receiveIntent.getExtras();
+                    //Create a new intent to the next page.
+                    Intent toPage3 = new Intent(DoctorPageSearch.this, Confirm.class);
+                    //Put the bundle from the previous page and info from this page in the intent.
+                    toPage3.putExtra("Page 1 Info", bundle);
+                    toPage3.putExtra("Page 2 Info", "Page 2");
+                    //Start the activity to the next page.
+                    startActivity(toPage3);
+                }
+            });
+
+        }
     }
-}
+
+
+
